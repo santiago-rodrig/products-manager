097371cdfb67e6a0e9cd66441e5d326a672c5e61
santiago-rodrig <santo1996.29@gmail.com>
2020-12-08 15:44:54 -0400

    basic components and routing

---
+++ src/components/NuevoProducto.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NuevoProducto = () => {
+    return (
+        <h1>NuevoProducto component</h1>
+    )
+}
+
+export default NuevoProducto
---
+++ src/components/Productos.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productos = () => {
+    return (
+        <h1>Productos component</h1>
+    )
+}
+
+export default Productos
