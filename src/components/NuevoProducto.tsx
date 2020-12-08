1b8f258d968c55b0e96319c1d9d21d561f048dbd
santiago-rodrig <santo1996.29@gmail.com>
2020-12-08 15:50:09 -0400

    NuevoProducto contents

--- src/components/NuevoProducto.tsx
+++ src/components/NuevoProducto.tsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const NuevoProducto = () => {
     return (
-        <h1>NuevoProducto component</h1>
+        <div className={'row justify-content-center'}>
+            <div className={'col-md-8'}>
+               <div className={'card'}>
+                   <div className={'card-body'}>
+                       <h2 className={'text-center mb-4 font-weight-bold'}>
+                           Agregar Nuevo Producto
+                       </h2>
+                       <form>
+                           <div className={'form-group'}>
+                               <label>Nombre Producto</label>
+                               <input name={'nombre'} type={'text'} className={'form-control'} placeholder={'Nombre Producto'} />
+                           </div>
+                           <div className={'form-group'}>
+                               <label>Precio Producto</label>
+                               <input name={'precio'} type={'number'} className={'form-control'} placeholder={'Precio Producto'} />
+                           </div>
+                           <button type={'submit'} className={'btn btn-primary font-weight-bold text-uppercase d-block w-100'}>Agregar</button>
+                       </form>
+                   </div>
+               </div>
+            </div>
+        </div>
     )
 }
 
